adffc48ac0b3879610a0cf9aa8bd2a73c1cb9bd1
Katrya-Kovalova <katya2441@gmail.com>
2023-03-12 22:34:18 +0200

    weather-app

--- src/Weather.js
+++ src/Weather.js
@@ -1,35 +1,12 @@
-import { useState, CSSProperties } from "react";
-import ClipLoader from "react-spinners/ClipLoader";
+import React from "react";
+import Search from "./Search";
+import "./styles.css";
 
-const override: CSSProperties = {
-  display: "block",
-  margin: "0 auto",
-  borderColor: "gold",
-};
-
-function App() {
-  let [loading, setLoading] = useState(true);
-  let [color, setColor] = useState("#ffffff");
-
+export default function App() {
   return (
-    <div className="sweet-loading">
-      <button onClick={() => setLoading(!loading)}>Toggle Loader</button>
-      <input
-        value={color}
-        onChange={(input) => setColor(input.target.value)}
-        placeholder="Color of the loader"
-      />
-
-      <ClipLoader
-        color={color}
-        loading={loading}
-        cssOverride={override}
-        size={150}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      />
+    <div className="App">
+      <h1>Weather App</h1>
+      <Search />
     </div>
   );
 }
-
-export default App;
